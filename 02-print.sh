658117f2b5efcc63eda314e16cd8ea17dba2af07
CHANDRASEKHAR <diponugotichandu@gmail.com>
2023-10-11 23:28:38 +0530

    coloring

--- 02-print.sh
+++ 02-print.sh
@@ -8,9 +8,9 @@ echo Hello World
 # \e[COL means colors you want to print
 # Red(31) Green(32) Yellow(33) Blue(34) Magenta(35) Cyan(36)
 # \e[0m disable the coloring
-echo -e "\e[31m Hello Red \e[0m"
-echo -e "\e[32m Hello Green \e[0m"
-echo -e "\e[33m Hello Yellow \e[0m"
+echo -e "\e[31mHello Red\e[0m"
+echo -e "\e[32mHello Green\e[0m"
+echo -e "\e[33m Hello Yellow \e[Om"
 echo -e "\e[34m Hello Blue \e[0m"
 echo -e "\e[35m Hello Magenta \e[0m"
 echo -e "\e[36m Hello Cyan \e[0m"
